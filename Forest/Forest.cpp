560a5a34284c900dc256d141da7473dd83eff53f
Soonha Hwang <soonhah2@siebl-0224-32.ews.illinois.edu>
2021-04-15 05:48:43 -0500

    Implemented ranking filter

--- Forest/Forest.cpp
+++ Forest/Forest.cpp
@@ -1,13 +1,22 @@
 #pragma once
 #include "Forest.h"
-#include <map>
+#include <unordered_map>
 #include <vector>
+#include <algorithm>
 
-Node* Forest::Node::makeNode(long itemId){
-    Node* newnode = new Node;
+// Node* Forest::Node::makeNode(long itemId){
+//     Node* newnode = new Node;
+//     newnode->_itemId = itemId;
+//     newnode->_copIndex = 0;
+// }
+
+
+Forest::Node(long itemID){
     newnode->_itemId = itemId;
     newnode->_copIndex = 0;
 }
+
+Forest::Node(){}
 
 /*
 Set the limit for co-purchasing, this is the limit used for IDDFS
@@ -31,26 +40,53 @@ void Forest::readFromFile(string file){
         
         if(_itemsMap[a]){   //we previously already created this node in the forest
             if(!_itemsMap[b]){
-                createNode = makeNode(b);
+                createNode = new Forest::Node(b);
                 _itemsMap[b] = createNode;
             }
             map[a]->_connected.push_back(_itemsMap[b]);
             
         }else{              //this is our first time seeing this itemId
-            createNode = makeNode(a);
+            createNode = new Forest::Node(a);
             _itemsMap[a] = createNode;
             if(!_itemsMap[b]){
-                createNode = makeNode(b);
+                createNode = new Forest::Node(b);
                 _itemsMap[b] = createNode;
             }
             map[a]->_connected.push_back(_itemsMap[b]);
         }
+        
     }
 }
 
-void Forest::filter500(){
+void Forest::filter(int size){
     //I am thinking IDDFS will be called here
-    //this is probably just a loop going through all the items, getting their Node*, then calling IDDFS on those Node*
+    
+    for(auto &iter : _itemsMap){
+        long index = iter.first;
+        Node* node = iter.second;
+        node->_copIndex = IDDFS(node, _copLimit);
+    }
+
+    std::vector<std::pair<long, long>> rank;
+
+    for(auto &iter : _itemsMap){
+        long index = iter.first;                        //save the itemID in index
+        long copIndex = iter.second->_copIndex;         //save copIndex here
+
+        if(rank.size()<size) rank.push_back(std::make_pair(copIndex, index)); //if vector is less than desired size, just push back
+        else{
+            sort(rank.rbegin(), rank.rend()); //sort to check if the node we are pushing is greater than min.
+
+            if(copIndex > rank[0].first){   //if so, pop back and replace
+                rank.pop_back());
+                rank.push_back(std::make_pair(copIndex, index));
+            }
+        }
+    }
+
+    for(auto &iter : rank){
+        _top500.push_back(iter.second); //push in the rank of the index. Now, _top500 will store the items with highest copIndex
+    }
 }
 
 /*
@@ -59,17 +95,18 @@ Set the count of distinct linked nodes within the Node class's <_copIndex>
 Return count
 Is it possible(easier) to implement using recursion? 
 */
-int IDDFS(Node* start, int limit){
+int Forest::IDDFS(Node* start, int limit){
     int count = 0;
     if(limit == 1){
         count = start->_connected.size();
         //start->_copIndex = count;
         return count;
     }
-
+    
     for(unsigned long i = 0; i<start->_connected.size(); i++){
-        Node* n = start->_connected[i];
-        count = count + IDDFS(n, limit-1);
-    }
+            Node* n = start->_connected[i];
+            count = count + IDDFS(n, limit-1);
+        }
     return count;
 }
+
--- Forest/Forest.h
+++ Forest/Forest.h
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<vector>
-#include<map>
+#include<unordered_map>
 #include<fstream>
 
 using namespace std;
@@ -11,15 +11,16 @@ class Forest{
             long _itemId;
             long _copIndex;
             vector<Node*> _connected;
-            Node* makeNode(long itemId);
+            Node(long itemId);
+            Node();
         }
         void readFromFile(string file);
         void setCopLimit(int limit);
-        void filter500();
+        void filter(int size);
         int IDDFS(Node* start, int limit);
     
     private:
-        map<long, Node*> _itemsMap;
-        vector<long> _top500;   //this is to store the itemId of the 500 most optimum products to be cut down to
+        std::unordered_map<long, Node*> _itemsMap;
+        std::vector<long> _top500;   //this is to store the itemId of the 500 most optimum products to be cut down to
         int _copLimit;          //this should by default be 0, this is the limit we set for IDDFS
 }
