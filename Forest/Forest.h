48b24bcc3aa4f26f17d79c8890ec10b0be32a8f1
Soonha Hwang <soonhah2@siebl-0224-28.ews.illinois.edu>
2021-04-18 09:21:04 -0500

    testcases

--- Forest/Forest.h
+++ Forest/Forest.h
@@ -17,13 +17,16 @@ class Forest{
             vector<Node*> _connected;
             Node(long itemId);
             Node();
+
+            private:
         };
 
         Forest();
         Forest(int copLimit);
         void readFromFile(string file);
         void filter(size_t size);
-        int IDDFS(Node* start, int limit);
+        int IDDFS(Node* start, int limit, unordered_map<Node*, bool> & bookkeep);
+        // int IDDFS(Node* start, int limit);
         
         int get_copLimit();
         void set_itemsMap(long idx, Node* node);
@@ -33,4 +36,6 @@ class Forest{
         unordered_map<long, Node*> _itemsMap;
         vector<long> _top500;   //this is to store the itemId of the 500 most optimum products to be cut down to
         int _copLimit;          //this should by default be 0, this is the limit we set for IDDFS
+        
+
 };
