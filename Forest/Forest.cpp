a35546d02e783183fbedd2f77d5108a78dc0c7c2
Soonha Hwang <soonhah2@siebl-0224-38.ews.illinois.edu>
2021-04-22 04:27:01 -0500

    mid-project Checkin

--- Forest/Forest.cpp
+++ Forest/Forest.cpp
@@ -22,8 +22,8 @@ Set the limit for co-purchasing, this is the limit used for IDDFS
 */
 
 Forest::Node::Node(){
-    Node::_itemId = -1;
-    Node::_copIndex = -1;
+    Node::_itemId = -1; //set _itemID as default
+    Node::_copIndex = -1; //set _copIndex to be -1 as default
 }
 
 Forest::Node::Node(long itemId){
@@ -41,31 +41,31 @@ mapping each itemId to their pointer in the forest
 
 
 void Forest::readFromFile(string file){
-    std::ifstream myfile(file);
+    std::ifstream myfile(file); //read from file to myfile
     int a, b;
-    while(myfile >> a >> b){
-        if(_itemsMap.find(a) == _itemsMap.end()){
-            Node* createNode = new Forest::Node(a);
-            _itemsMap[a] = createNode;
+    while(myfile >> a >> b){ //store first column to a, second to b with tab separated
+        if(_itemsMap.find(a) == _itemsMap.end()){ //if we have not created a node yet
+            Node* createNode = new Forest::Node(a); //create node
+            _itemsMap[a] = createNode;  //allocate the address of node to the _itemsMap
         }
-        if(_itemsMap.find(b) == _itemsMap.end()){
-            Node* createNode = new Forest::Node(b);
-            _itemsMap[b] = createNode;
+        if(_itemsMap.find(b) == _itemsMap.end()){ //if we have not created b node yet
+            Node* createNode = new Forest::Node(b); //create node
+            _itemsMap[b] = createNode; //allocate the address of b node to the _itemsMap
         }
-        _itemsMap[a]->_connected.push_back(_itemsMap[b]);
-    }
+        _itemsMap[a]->_connected.push_back(_itemsMap[b]); //push the address of b to _connected vector of _itemsmap[a]
+    } 
 }
 
-void Forest::filter(size_t size){
-    for(auto &iter : _itemsMap){
-        long index = iter.first;
-        Node* node = iter.second;
-        node->_copIndex = IDDFS(node, _copLimit);
+void Forest::filter(size_t size){ //size is the number of rankings we want to get. i.e. size = 30, we get top 30 products of highest copIndex
+    for(auto &iter : _itemsMap){ //iterate through _itemsMap
+        long index = iter.first; //we know first element of itemsmap is the index(itemID)
+        Node* node = iter.second;//second is the address of that index
+        node->_copIndex = IDDFS(node, _copLimit); //set copIndex of the node by running Depth Limited Search
     }
 
-    std::vector<std::pair<long, long>> rank;
+    std::vector<std::pair<long, long>> rank; //make a map of rank to help us rank the products according to copIndex
 
-    for(auto &iter : _itemsMap){
+    for(auto &iter : _itemsMap){ //Iterate through _itemsMap
 
         long index = iter.first;                        //save the itemID in index
         long copIndex = iter.second->_copIndex;         //save copIndex here
@@ -83,14 +83,14 @@ void Forest::filter(size_t size){
                 rank.pop_back();
                 rank.push_back(std::make_pair(copIndex, index));
             }
-            std::cout<<"Current Rank:";
+            std::cout<<"Current Rank:"; //just print to debug
             for(size_t i = 0; i<size; i++){
-                std::cout<<rank[i].second<<" ";
+                std::cout<<rank[i].second<<" "; //just print to debug
             }
-            std::cout<<std::endl;
+            std::cout<<std::endl; //just print to debug
         }
     }
-    sort(rank.rbegin(), rank.rend()); 
+    sort(rank.rbegin(), rank.rend()); //sort from largest to smallest in rank
 
     for(auto &iter : rank){
         _topProduct.push_back(iter.second); //push in the rank of the index. Now, _top500 will store the items with highest copIndex
@@ -104,30 +104,37 @@ Return count
 Is it possible(easier) to implement using recursion? 
 */
 int Forest::IDDFS(Node* start, int limit){
-    unordered_map<Forest::Node*, bool> visited;
+    unordered_map<Forest::Node*, bool> visited; //created map of visited to perfrom the recursive iteration
     return IDDFS_helper(start, start, limit, visited);
 }
 
 int Forest::IDDFS_helper(Node* start, Node* cur, int limit, unordered_map<Node*, bool>& bookkeep){
-    int count = 0;
+    /*
+    Node* start     : starting address of the Depth limited search (DLS)
+    Node* cur       : current address we are iterating
+    int limit       : limit we will set in our depth limited search
+    map<> bookkeep  : checks which nodes are visited and which are not to prevent crash
+    */
 
-    if(limit == 1){
-        bookkeep[cur] = 1;
-        count = cur->_connected.size();
-        for(unsigned long i = 0; i<cur->_connected.size(); i++){
+    int count = 0; //count is number of nodes it is connected to.
+
+    if(limit == 1){ //base case
+        bookkeep[cur] = 1; 
+        count = cur->_connected.size(); //set count to be the number of connected nodes
+        for(unsigned long i = 0; i<cur->_connected.size(); i++){ //delete all the duplicating nodes
             if(bookkeep[cur->_connected[i]]) count--;
             bookkeep[cur->_connected[i]] = 1;
         }
         return count;
     }
     
-    if(limit == 0) return 0;
+    if(limit == 0) return 0; //if limit = 0 simply return 0
 
-    bookkeep[cur] = 1;
+    bookkeep[cur] = 1; //set the node as visited
     count += cur->_connected.size();          //for every node we visit, we need to add its branching factor
     for(unsigned long i = 0; i<cur->_connected.size(); i++){
-        if(bookkeep[cur->_connected[i]]) count--;
-        bookkeep[cur->_connected[i]] = 1;
+        if(bookkeep[cur->_connected[i]]) count--; //if we have visited the node, we decrement the count
+        // bookkeep[cur->_connected[i]] = 1; 
         count = count + IDDFS_helper(start, cur->_connected[i], limit-1, bookkeep);   //perform IDDFS on each immediately neighboring node
     }
     return count;
--- Forest/Forest.h
+++ Forest/Forest.h
@@ -11,27 +11,26 @@ using namespace std;
 class Forest{
     public:
         struct Node{
-            long _itemId;
-            long _copIndex;
+            long _itemId; //Product ID (First column of txt file)
+            long _copIndex; //Will store _copIndex here
 
-            vector<Node*> _connected;
-            Node(long itemId);
-            Node();
+            vector<Node*> _connected; //vector of connected nodes
+            Node(long itemId); //constructor of Node struct
+            Node();             //default constructor
 
-            private:
         };
 
-        Forest();
-        Forest(int copLimit);
-        void readFromFile(string file);
-        void filter(size_t size);
-        int IDDFS(Node* start, int limit);
-        int IDDFS_helper(Node* start, Node* cur, int limit, unordered_map<Node*, bool>& visited);
+        Forest(); //Default constructor of Forest class
+        Forest(int copLimit); //constructor of forest class that sets copLimit
+        void readFromFile(string file); //function to read from txt file and generate all the nodes
+        void filter(size_t size);   //function to filter top-N products
+        int IDDFS(Node* start, int limit); //Performs Depth-Limited Search to find copindex
+        int IDDFS_helper(Node* start, Node* cur, int limit, unordered_map<Node*, bool>& visited); //helper function of IDDFS
         
-        int get_copLimit();
-        void set_itemsMap(long idx, Node* node);
-        unordered_map<long, Node*> get_itemsMap();
-        vector<long> get_topProduct();
+        int get_copLimit();         //returns _copIndex
+        void set_itemsMap(long idx, Node* node); //helps create _itemsMap
+        unordered_map<long, Node*> get_itemsMap(); // returns _itemsMap
+        vector<long> get_topProduct(); //returns _topProduct
 
     private:
         unordered_map<long, Node*> _itemsMap;
